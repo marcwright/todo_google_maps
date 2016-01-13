293be0817e12faf6831a18674e2e9db0913ff4d0
marcwright <marc.wright@gmail.com>
2016-01-13 14:55:03 -0500

    Created MVC CRUD for a list of TODOs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :todos
   root to: 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
+  match 'todos/:id/toggle_completed', to: 'todos#toggle_completed', via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
