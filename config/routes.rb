2545ead474c6627b5e6a78847e5fb202b588737f
marcwright <marc.wright@gmail.com>
2016-01-13 16:08:27 -0500

    Create the Sessions Controller, Sessions Helper, and navigation links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :sessions, only:[:new, :create, :destroy]
+
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   resources :users
   resources :todos
-  root to: 'static_pages#home'
+  root 'static_pages#home'
   match '/about', to: 'static_pages#about', via: 'get'
   match 'todos/:id/toggle_completed', to: 'todos#toggle_completed', via: 'get'
 
