ab948983a2ca4892cc9c1a6376f78ae01b9965dc
marcwright <marc.wright@gmail.com>
2016-01-14 19:07:21 -0500

    changed google maps back to http

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module TodoAppAuth
       g.template_engine :erb
     end
 
-    config.serve_static_assets = true
+    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
