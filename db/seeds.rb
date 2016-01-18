0ceb88fa6b148247a4902080a8a6b5ea8d1a4ae9
marcwright <marc.wright@gmail.com>
2016-01-18 16:12:03 -0500

    updated seeds with img_url

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Assignee.destroy_all
 Assignee.create([
-    {name: "Marc", address: "477 Wilmer St NE", city: "Atlanta", state: "GA", what: "assignee"},
-    {name: "Maren", address: "695 North Avenue NE", city: "Atlanta", state: "GA", what: "assignee"},
-    {name: "Katy", address: " 725 Ponce De Leon Ave NE", city: "Atlanta", state: "GA", what: "assignee"}
+    {name: "Marc", address: "477 Wilmer St NE", city: "Atlanta", state: "GA", what: "assignee", img_url: "assets/images/marc_pic30.jpg"},
+    {name: "Maren", address: "695 North Avenue NE", city: "Atlanta", state: "GA", what: "assignee", img_url: "assets/images/maren_pic30.jpg"},
+    {name: "Katy", address: " 725 Ponce De Leon Ave NE", city: "Atlanta", state: "GA", what: "assignee", img_url: "assets/images/katy_pic30.jpg"}
    ])
 
 Todo.destroy_all
