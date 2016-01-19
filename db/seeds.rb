844e2c0eef57cbb343bbd52c27a6e67a40c6803c
marcwright <marc.wright@gmail.com>
2016-01-18 19:37:39 -0500

    about to create branch for Assignees index map

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,9 @@
 Assignee.destroy_all
 Assignee.create([
     {name: "Marc", address: "477 Wilmer St NE", city: "Atlanta", state: "GA", what: "assignee", img_url: "assets/images/marc_pic30.jpg"},
-    {name: "Maren", address: "695 North Avenue NE", city: "Atlanta", state: "GA", what: "assignee", img_url: "assets/images/maren_pic30.jpg"},
-    {name: "Katy", address: " 725 Ponce De Leon Ave NE", city: "Atlanta", state: "GA", what: "assignee", img_url: "assets/images/katy_pic30.jpg"}
+    {name: "Maren", address: "1045 North Highland Avenue NE", city: "Atlanta", state: "GA", what: "assignee", img_url: "assets/images/maren_pic30.jpg"},
+    {name: "Katy", address: "1045 Piedmont Ave NE", city: "Atlanta", state: "GA", what: "assignee", img_url: "assets/images/katy_pic30.jpg"},
+    {name: "Schmitty", address: "255 5th St NW", city: "Atlanta", state: "GA", what: "assignee", img_url: "assets/images/katy_pic30.jpg"}
    ])
 
 Todo.destroy_all
